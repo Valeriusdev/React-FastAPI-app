54a27e44409d217b171c4357efcb44579445ffcf
Val Lysenko <kakao757@gmail.com>
2026-03-14 19:45:36 -0700

    setup react app with tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="grid place-items-center min-h-screen">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
