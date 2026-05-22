9982135f19278d8881c9b17d0381bc7254973cfe
Val Lysenko <kakao757@gmail.com>
2026-05-21 21:21:26 -0700

    update styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import BookList from "./components/Books";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Library App</h1>
+      <h1 className="text-3xl font-bold mb-6">Library App</h1>
       <BookList />
     </div>
   );
